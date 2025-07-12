d53ceb99208e4950b9e1d137d7e508f5e531d6c9
Ilya Miklashevich <miklash.ilya.serg@gmail.com>
2025-07-12 18:31:43 +0300

    Добавлена базовая реализация файла Program для работы сервера.

--- SignalRAppChat/Program.cs
+++ SignalRAppChat/Program.cs
@@ -1,6 +1,13 @@
+using SignalRAppChat;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapHub<ChatHub>("/chat");
 
 app.Run();

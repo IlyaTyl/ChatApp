13e7456f80015ab4deaefbb6c80bc401865c0982
Ilya Miklashevich <miklash.ilya.serg@gmail.com>
2025-07-12 20:24:36 +0300

    Добавление окна авторизации AuthRegWindow.

---
+++ WpfClientChat/AuthRegWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfClientChat
+{
+    public partial class AuthRegWindow : Window
+    {
+        private HubConnection connection;
+
+        public string AuthenticatedUsername { get; private set; }
+        public AuthRegWindow()
+        {
+            InitializeComponent();
+            connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:7226/chat")
+                .WithAutomaticReconnect()
+                .Build();
+
+            StartConnection();
+        }
+
+        private async void StartConnection()
+        {
+            try
+            {
+                await connection.StartAsync();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Не удалось подключиться к серверу: {ex.Message}");
+            }
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

13c8f82dd017c18de05026e48dc284accdffb703
Ilya Miklashevich <miklash.ilya.serg@gmail.com>
2025-07-12 19:31:53 +0300

    Добавлена сущность Message, изменен модификатор доступа internal на public у класса User.

--- SignalRAppChat.Shared/Models/Message.cs
+++ SignalRAppChat.Shared/Models/Message.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace SignalRAppChat.Shared.Models
 {
-    public class User
+    public class Message
     {
         public int Id { get; set; }
         public string UserName { get; set; } = null!;
-        public string Password { get; set; } = null!;
+        public string Text { get; set; } = null!;
+        public DateTime SentAt { get; set; } = DateTime.UtcNow;
     }
 }

a175ba4bfc060dace154ba587bf9dcc6e4c5663c
Ilya Miklashevich <miklash.ilya.serg@gmail.com>
2025-07-12 19:19:22 +0300

    Добавление сущности User

---
+++ SignalRAppChat.Shared/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRAppChat.Shared.Models
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}

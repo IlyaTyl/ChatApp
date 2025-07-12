67d5961cc35680a16c465336c0422eef283a7cce
Ilya Miklashevich <miklash.ilya.serg@gmail.com>
2025-07-12 20:02:31 +0300

    Добавлено подключение к БД в файле Program для серверной части.

--- SignalRAppChat/Program.cs
+++ SignalRAppChat/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
 using SignalRAppChat;
+using SignalRAppChat.Data;
+
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSignalR();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 

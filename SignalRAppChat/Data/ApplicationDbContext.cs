f853a35736b368f15598ff067fbef9384a1d43f2
Ilya Miklashevich <miklash.ilya.serg@gmail.com>
2025-07-12 19:44:14 +0300

    Добавление контекста ApplicationDbContext в проект серверной части и добавление ссылки на проект SignalRApp.Shared для серверной и клиентской части.

---
+++ SignalRAppChat/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SignalRAppChat.Shared.Models;
+
+namespace SignalRAppChat.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+    }
+}

3f94bbb2d3913fe32fcdfd4da187f16615eb8dfe
Ilya Miklashevich <miklash.ilya.serg@gmail.com>
2025-08-16 14:30:25 +0300

    Добавлена разметка окна для предосмотра изображений перед отправкой.

---
+++ WpfClientChat/ImagePreviewWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfClientChat
+{
+    /// <summary>
+    /// Логика взаимодействия для ImagePreviewWindow.xaml
+    /// </summary>
+    public partial class ImagePreviewWindow : Window
+    {
+        public ImagePreviewWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
